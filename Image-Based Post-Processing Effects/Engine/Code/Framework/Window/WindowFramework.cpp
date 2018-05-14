3c3e31f307cf805387a4a182f88cf5280e0e39fc
Tim Dörries <doerriet@hochschule-trier.de>
2018-05-14 19:21:33 +0200

    -switched to GL 4.5

--- Image-Based Post-Processing Effects/Engine/Code/Framework/Window/WindowFramework.cpp
+++ Image-Based Post-Processing Effects/Engine/Code/Framework/Window/WindowFramework.cpp
@@ -37,8 +37,8 @@ WindowFramework::~WindowFramework()
 void WindowFramework::init()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_AUTO_ICONIFY, GLFW_FALSE);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
